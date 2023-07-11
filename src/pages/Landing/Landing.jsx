b768501e5b5948dc2016c56149bc7d41ece9035a
Jesse Uszkay <jesseuszkay@Jims-MacBook-Pro.local>
2023-07-11 15:15:02 -0400

    preload bg for desktop

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Landing.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Landing() {
+  const navigate = useNavigate();
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = "../src/assets/hero.jpg";
+    image.addEventListener("load", () => {
+      setImageLoaded(true);
+    });
+  }, []);
+
+  const handleEnterClick = () => {
+    navigate("/home");
+  };
+
   return (
     <div className="landing">
       <div className="landing__content">
@@ -10,9 +26,18 @@ function Landing() {
           <span className="landing__title landing__title--2">to</span>
           <span className="landing__title landing__title--3">karaoke</span>
         </div>
-        <Link to="/home" className="landing__button">
-          ENTER SITE
-        </Link>
+        <div>
+          {imageLoaded && (
+            <div onClick={handleEnterClick} className="landing__button">
+              Enter Site
+            </div>
+          )}
+          <img
+            src="../src/assets/hero.jpg"
+            alt="Background"
+            style={{ display: "none" }}
+          />
+        </div>
       </div>
     </div>
   );
