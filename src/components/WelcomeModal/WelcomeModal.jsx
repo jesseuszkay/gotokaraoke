cac6ad40d6192194b4df75cc1a5d6a9dd940b3c4
Jesse Uszkay <jesseuszkay@gmail.com>
2023-07-17 18:54:09 -0400

    separate modals into separate components

---
+++ src/components/WelcomeModal/WelcomeModal.jsx
@@ -0,0 +1,85 @@
+import tunnel from "../../assets/tunnel.mp4";
+import tunnelVertical from "../../assets/tunnel-vertical.mp4";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import "./WelcomeModal.scss";
+
+function WelcomeModal({ welcome, setWelcome }) {
+  const handleClose = () => {
+    setWelcome(false);
+    sessionStorage.setItem("seenWelcomeModal", true);
+  };
+
+  return (
+    <div className="welcome-modal">
+      {!sessionStorage.getItem("seenWelcomeModal") && (
+        <Modal
+          open={welcome}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          sx={{
+            // Added inline styling to override the default border
+            border: "none",
+          }}
+        >
+          <Box
+            id="welcome-modal"
+            sx={{
+              // Added inline styling to override the default border
+              border: "none",
+            }}
+          >
+            <div className="welcome-modal-box">
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="video video--mobile"
+              >
+                <source src={tunnelVertical} type="video/mp4" />
+              </video>
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="video video--desktop"
+              >
+                <source src={tunnel} type="video/mp4" />
+              </video>
+              <IconButton
+                id="welcome-modal__close-button"
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </IconButton>
+              <div className="welcome-modal-text">
+                <Typography id="welcome-modal__title">
+                  Welcome to gotokaraoke
+                </Typography>
+                <Typography id="welcome-modal__description">
+                  Urgently need the perfect karaoke song? No problem! Use
+                  gotokaraoke's expert song finder to search, filter, and scroll
+                  through our curated karaoke tracks. Click on a song title to
+                  open the karaoke version on Youtube!
+                </Typography>
+                <Typography id="welcome-modal__description">
+                  Got some time to prep for your next karaoke session? Create an
+                  account to save your karaoke song picks so that they are
+                  always ready when you need them.
+                </Typography>
+              </div>
+            </div>
+          </Box>
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default WelcomeModal;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,23 @@
 import SongFinder from "../../components/SongFinder/SongFinder";
 import AlbumGrid from "../../components/AlbumGrid/AlbumGrid";
 import helpButton from "../../assets/help.png";
-import tunnel from "../../assets/tunnel.mp4";
-import tunnelVertical from "../../assets/tunnel-vertical.mp4";
 import { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
 import "./Home.scss";
-import "../../components/WelcomeModal/WelcomeModal.scss";
 import HelpModal from "../../components/HelpModal/HelpModal";
+import WelcomeModal from "../../components/WelcomeModal/WelcomeModal";
 
 function Home({ isLoggedIn, userDetails, setUserDetails, apiURL }) {
   const [gridMode, setGridMode] = useState(false);
-  const [open, setOpen] = useState(true);
+  const [welcome, setWelcome] = useState(true);
   const [help, setHelp] = useState(false);
 
   useEffect(() => {
-    const seenModal = sessionStorage.getItem("seenModal");
-    if (!seenModal) {
-      setOpen(true);
-      sessionStorage.setItem("seenModal", true);
+    const seenWelcomeModal = sessionStorage.getItem("seenWelcomeModal");
+    if (!seenWelcomeModal) {
+      setWelcome(true);
+      sessionStorage.setItem("seenWelcomeModal", true);
     }
   }, []);
-
-  const handleClose = () => {
-    setOpen(false);
-    sessionStorage.setItem("seenModal", true);
-  };
 
   function handleOnClick(event) {
     if (event.target.value === "list") {
@@ -59,67 +47,7 @@ function Home({ isLoggedIn, userDetails, setUserDetails, apiURL }) {
         />
         {help && <HelpModal help={help} setHelp={setHelp} />}
         {!sessionStorage.getItem("seenModal") && (
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            sx={{
-              // Added inline styling to override the default border
-              border: "none",
-            }}
-          >
-            <Box
-              id="welcome-modal"
-              sx={{
-                // Added inline styling to override the default border
-                border: "none",
-              }}
-            >
-              <div className="welcome-modal-box">
-                <video
-                  autoPlay
-                  muted
-                  loop
-                  playsInline
-                  className="video video--mobile"
-                >
-                  <source src={tunnelVertical} type="video/mp4" />
-                </video>
-                <video
-                  autoPlay
-                  muted
-                  loop
-                  playsInline
-                  className="video video--desktop"
-                >
-                  <source src={tunnel} type="video/mp4" />
-                </video>
-                <IconButton
-                  id="welcome-modal__close-button"
-                  onClick={handleClose}
-                >
-                  <CloseIcon />
-                </IconButton>
-                <div className="welcome-modal-text">
-                  <Typography id="welcome-modal__title">
-                    Welcome to gotokaraoke
-                  </Typography>
-                  <Typography id="welcome-modal__description">
-                    Urgently need the perfect karaoke song? No problem! Use
-                    gotokaraoke's expert song finder to search, filter, and
-                    scroll through our curated karaoke tracks. Click on a song
-                    title to open the karaoke version on Youtube!
-                  </Typography>
-                  <Typography id="welcome-modal__description">
-                    Got some time to prep for your next karaoke session? Create
-                    an account to save your karaoke song picks so that they are
-                    always ready when you need them.
-                  </Typography>
-                </div>
-              </div>
-            </Box>
-          </Modal>
+          <WelcomeModal welcome={welcome} setWelcome={setWelcome} />
         )}
         <div className="home__selects">
           <button
